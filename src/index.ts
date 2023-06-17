9d22d5ffcef2e4782c2a716692015183532c2b12
Dawn Sheedy <dawn@dawnsheedy.com>
2023-06-16 20:33:18 -0400

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Express } from "express"
+const express = require('express')
+const app: Express = express()
+const port = process.env.PORT ?? 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
